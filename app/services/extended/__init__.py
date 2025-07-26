4af21b35d11234103fba6f22d9d12144e76cc134
Renzo Barcos <renzoespinosa.b@gmail.com>
2025-07-25 23:32:19 -0300

    refactor: update extended service imports for Supabase compatibility

--- app/services/extended/__init__.py
+++ app/services/extended/__init__.py
@@ -1,5 +1,5 @@
 """Extended Exchange API client module"""
 from app.services.extended.client import ExtendedExchangeClient
-
+ 
 # Create global client instance
 extended_client = ExtendedExchangeClient() 
